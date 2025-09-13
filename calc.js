23c7419cacb0809f5d6e62d57af09dbfc8c1121c
ruchit <example@gmail.com>
2025-09-13 19:40:08 +0530

    ON/OFF

--- calc.js
+++ calc.js
@@ -8,16 +8,38 @@ function print(e){
     let value=e.target.innerText
     if(value==="OFF") off()
     if(value==="ON") on()
+    if(parseInt(value) || value==="0" || value==="00" || value==='.') number(value)
+    if(value==="/" || value==="*" || value==="-" || value==='+') result(value)
+    if(value==="=") computation()
+    if(value==="DEL") backspace()
+    if (value==="AC") clear()
 }
 
+let num=''
+function number(value){
+    num=num+value
+    input.innerText=num
+}
 
+function result(value){
+    num=num+value
+    input.innerText=num
+}
 
+function computation(){
+    input.innerText=eval(num)
+    num=''
+}
 
+function clear(){
+    num=''
+    input.innerText=''
+}
 
-
-
-
-
+function backspace(){
+    num=num.slice(0,-1)
+    input.innerText=num
+}
 
 function off(){
     button.forEach((i)=>{
