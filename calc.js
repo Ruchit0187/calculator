bfa8adcc484cf4f4ee2855c2935e68cdd7e0160c
ruchit <example@gmail.com>
2025-09-13 23:52:05 +0530

    power

--- calc.js
+++ calc.js
@@ -1,35 +1,80 @@
 let input=document.querySelector(".input")
 let button=document.querySelectorAll(".btn");
+let root=document.querySelector(".root").innerText;
+
+
 button.forEach((i)=>{
     i.addEventListener('click',print)
 })
 
 function print(e){
-    let value=e.target.innerText
+    let value=e.target.innerText;
     if(value==="OFF") off()
     if(value==="ON") on()
     if(parseInt(value) || value==="0" || value==="00" || value==='.') number(value)
     if(value==="/" || value==="*" || value==="-" || value==='+') result(value)
-    if(value==="=") computation()
+    if(value==="=")  Computation()
     if(value==="DEL") backspace()
     if (value==="AC") clear()
+    if(value==="^") number()
+    if(value==="Sq")  Squre()
+    if(value===root) Squreroot()
 }
-
 let num=''
 function number(value){
+    if(value===undefined){
+        value='^'
+    }
     num=num+value
     input.innerText=num
 }
 
 function result(value){
+    if(num.length>0){
     num=num+value
     input.innerText=num
+    }
+    else{
+        alert("enter the number")
+    }
 }
 
-function computation(){
+function  Computation(){
+    if(num.length===0){
+        alert("give the number and operation")
+        return
+    }
+    if(num.includes('^')){
+        let a=num.split("^");
+        input.innerText=Math.pow(eval(a[0]),eval(a[1]))
+        return
+    }
     input.innerText=eval(num)
-    num=''
+    num=input.innerText
 }
+function power(){
+  let sp=num.split('^');
+  console.log(sp)
+}
+function Squre(){
+     if(num.length>0){
+    num=num+'**2'
+    Computation()
+    }
+     else{
+        alert("enter the number")
+     }
+}
+function Squreroot(){
+    if(num.length>0){
+    num=num+'**0.5'
+    Computation()
+    }
+    else{
+        alert("enter the number")
+    }
+}
+
 
 function clear(){
     num=''
